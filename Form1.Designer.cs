a8b58f824ad83190ae9fa04980017314e9aed790
ADMIN <ADMIN@LAB1-MAY19>
2023-09-18 15:58:31 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace Csharp_baitap10
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnShow = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.txtYourName = new System.Windows.Forms.TextBox();
+            this.txtYearofbirth = new System.Windows.Forms.TextBox();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(71, 80);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(105, 22);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Your Name ";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(71, 177);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(116, 22);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Year of brith ";
+            // 
+            // btnShow
+            // 
+            this.btnShow.Location = new System.Drawing.Point(149, 281);
+            this.btnShow.Name = "btnShow";
+            this.btnShow.Size = new System.Drawing.Size(117, 40);
+            this.btnShow.TabIndex = 4;
+            this.btnShow.Text = "&Show ";
+            this.btnShow.UseVisualStyleBackColor = true;
+            this.btnShow.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(359, 281);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(117, 40);
+            this.btnClear.TabIndex = 5;
+            this.btnClear.Text = "&Clear ";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(571, 281);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(117, 40);
+            this.btnExit.TabIndex = 6;
+            this.btnExit.Text = "E&xit ";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // txtYourName
+            // 
+            this.txtYourName.Location = new System.Drawing.Point(277, 82);
+            this.txtYourName.Name = "txtYourName";
+            this.txtYourName.Size = new System.Drawing.Size(330, 20);
+            this.txtYourName.TabIndex = 1;
+            this.txtYourName.TextChanged += new System.EventHandler(this.txtYourName_TextChanged);
+            this.txtYourName.Leave += new System.EventHandler(this.txtYourName_Leave);
+            // 
+            // txtYearofbirth
+            // 
+            this.txtYearofbirth.Location = new System.Drawing.Point(277, 179);
+            this.txtYearofbirth.Name = "txtYearofbirth";
+            this.txtYearofbirth.Size = new System.Drawing.Size(330, 20);
+            this.txtYearofbirth.TabIndex = 3;
+            this.txtYearofbirth.TextChanged += new System.EventHandler(this.txtYearofbirth_TextChanged);
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtYearofbirth);
+            this.Controls.Add(this.txtYourName);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnShow);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "qua du luon";
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnShow;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.TextBox txtYourName;
+        private System.Windows.Forms.TextBox txtYearofbirth;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Csharp_baitap10
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            DialogResult r;
+            r = MessageBox.Show("Do you want really want to close ?", "Ex1",
+                               MessageBoxButtons.YesNo,
+                               MessageBoxIcon.Question,
+                               MessageBoxDefaultButton.Button1);
+            if (r == DialogResult.No)
+                e.Cancel = true;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int age;
+            String s;
+            s = "My name is: " + txtYourName.Text + "\n";
+            age = DateTime.Now.Year - Convert.ToInt32(txtYearofbirth.Text);//Current Year - Enter year
+            s = s + "Age: " + age.ToString();
+            MessageBox.Show(s);
+
+        }
+
+        private void txtYourName_TextChanged(object sender, EventArgs e)
+        {
+            Control ctr = (Control)sender;
+            if (ctr.Text.Trim().Length == 0)
+                this.errorProvider1.SetError(ctr, "You must enter your name");
+            else
+                this.errorProvider1.Clear();
+ 
+        }
+
+        private void txtYearofbirth_TextChanged(object sender, EventArgs e)
+        {
+            Control ctr = (Control)sender;
+            if (ctr.Text.Length > 0 && !char.IsDigit(ctr.Text[ctr.Text.Length - 1]))
+                this.errorProvider1.SetError(ctr, "This is not avalid number ");
+            else
+                this.errorProvider1.Clear();
+
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+               this.Close();
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            //clear text 
+            txtYourName.Clear();
+            txtYearofbirth.Clear();
+            //textbox your name reset focus 
+            txtYourName.Focus();
+        }
+
+        private void txtYourName_Leave(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
